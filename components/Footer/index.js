3fdc264b525086d2d7ce7a0706dcfe9aca1dd110
IamThiago-IT <thiagodossantos315@gmail.com>
2020-10-10 09:44:33 -0300

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <div className='bg-gray-700 p-4'>
       <div className='container mx-auto text-center font-bold text-white'>
         Projeto desenvolvido por: {' '}
-        <a className='hover:underline' href='https://tuliofaria.net'>Tulio Faria</a> / {' '}
-        <a className='hover:underline' href='https://linkedin.com/in/tuliofaria'>Linkedin</a> / {' '}
-        <a className='hover:underline' href='https://github.com/tuliofaria'>Github</a>
+        <a className='hover:underline' href='#'>Thiago dos Santos</a> / {' '}
+        <a className='hover:underline' href='https://linkedin.com/in/iamthiagoff'>Linkedin</a> / {' '}
+        <a className='hover:underline' href='https://github.com/IamThiago-IT'>Github</a>
         <div className='mt-2'>
           <img className='inline p-4' src='/logo_semana_fsm.png' />
           <img className='inline p-4' src='/logo_devpleno.png' />
